a4483ed8d08a2c69c582ca9f1fe26c87c40b1d7a
c.ilgner <c.ilgner@tmg-web.de>
2024-01-03 12:50:42 +0100

    Handle NoConnectionException and SQLException in main method
    The main method in the Main.java file has been updated to handle NoConnectionException and SQLException. Similar exceptions handling is implemented in MysqlConnection.java constructor. This update enhances error handling in the connection and database interaction processes to prevent unforeseen crashes due to unhandled exceptions.

--- src/main/java/de/goldendeveloper/guildmanager/MysqlConnection.java
+++ src/main/java/de/goldendeveloper/guildmanager/MysqlConnection.java
@@ -4,6 +4,9 @@ import de.goldendeveloper.guildmanager.errors.CustomExceptionHandler;
 import de.goldendeveloper.mysql.MYSQL;
 import de.goldendeveloper.mysql.entities.Database;
 import de.goldendeveloper.mysql.entities.Table;
+import de.goldendeveloper.mysql.exceptions.NoConnectionException;
+
+import java.sql.SQLException;
 
 public class MysqlConnection {
 
@@ -14,7 +17,7 @@ public class MysqlConnection {
     public static final String colmWChannel = "WelcomeChannel";
     public static final String colmJRole = "JoinRole";
 
-    public MysqlConnection(String hostname, String username, String password, int port) {
+    public MysqlConnection(String hostname, String username, String password, int port) throws NoConnectionException, SQLException {
         mysql = new MYSQL(hostname, username, password, port, new CustomExceptionHandler());
         if (!mysql.existsDatabase(dbName)) {
             mysql.createDatabase(dbName);
